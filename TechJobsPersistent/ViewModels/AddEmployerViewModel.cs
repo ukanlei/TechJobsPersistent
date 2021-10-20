2612bf7c76bd8c0d4f677a83e5bf7ad33fcb291a
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-10-19 23:15:25 -0500

    Create view model with two properties(name and location)  with validations

---
+++ TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TechJobsPersistent.Models;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddEmployerViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Locaiont is required")]
+        public string Location { get; set; }
+    }
+}
