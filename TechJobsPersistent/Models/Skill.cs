f5eb9f0dbc4bab19381c6185c5cb51b4cb4ca0ab
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-10-23 03:50:48 -0500

    Add validation to Skill form

--- TechJobsPersistent/Models/Skill.cs
+++ TechJobsPersistent/Models/Skill.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace TechJobsPersistent.Models
 {
     public class Skill
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Skill name is required")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
 
         public Skill()
